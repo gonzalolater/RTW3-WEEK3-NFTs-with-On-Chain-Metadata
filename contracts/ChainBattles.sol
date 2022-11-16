f99f67cab19072a8b6d58b95dbb608c454b76720
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-11-16 04:24:06 -0300

    changes

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -6,13 +6,61 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract ChainBattles is ERC721URIStorage  {
+contract ChainBattles is ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     mapping(uint256 => uint256) public tokenIdToLevels;
 
-    constructor() ERC721 ("Chain Battles", "CBTLS"){
+    function getLevels(uint256 tokenId) public view returns (string memory) {
+        uint256 levels = tokenIdToLevels[tokenId];
+        return levels.toString();
     }
+
+    function getTokenURI(uint256 tokenId) public returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Chain Battles #',
+            tokenId.toString(),
+            '",',
+            '"description": "Battles on chain",',
+            '"image": "',
+            generateCharacter(tokenId),
+            '"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
+    }
+
+    function generateCharacter(uint256 tokenId) public returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Warrior",
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Levels: ",
+            getLevels(tokenId),
+            "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    constructor() ERC721("Chain Battles", "CBTLS") {}
 }
